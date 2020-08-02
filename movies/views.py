f19ed447a5dab9c6df063ef4131b57c241d7b4ad
Mateusz Pruski <pruskimateusz93@gmail.com>
2020-08-02 16:12:35 +0200

    Added views.

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def movies_search_view(request, *args, **kwargs):
+    return render(request, 'movies_search.html', context={})
+
+
+def movies_rate_view(request, *args, **kwargs):
+    return render(request, 'movies_rate.html', context={})
+
+
+def movies_watch_view(request, *args, **kwargs):
+    return render(request, 'movies_watch.html', context={})
